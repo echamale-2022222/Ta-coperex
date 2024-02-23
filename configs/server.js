1f0d5cdf357a59f63ce67b49b192ddafe2dbe7ec
echamale-2022222 <echamale-2022222@kinal.edu.gt>
2024-02-23 17:28:46 -0600

    The admin login and the generation of the token that will be used later were completed

--- configs/server.js
+++ configs/server.js
@@ -5,12 +5,14 @@ import helmet from 'helmet'
 import morgan from 'morgan'
 import { dbConnection } from './mongo.js'
 import adminRoutes from '../src/admin/admin.routes.js'
+import authRoutes from '../src/auth/auth.routes.js'
 
 class Server{
     constructor(){
         this.app = express();
         this.port = process.env.PORT;
         this.adminPath = '/coperex/v1/admin';
+        this.authPath = '/coperex/v1/auth';
 
         this.middlewares();
         this.connectDB();
@@ -30,6 +32,7 @@ class Server{
     }
 
     routes(){
+        this.app.use(this.authPath, authRoutes);
         this.app.use(this.adminPath, adminRoutes);
     }
 
---
+++ src/middlewares/validate-jwt.js
@@ -0,0 +1,38 @@
+import jwt from "jsonwebtoken";
+import Admin from '../admin/admin.model';
+import { request, response } from 'express';
+
+export const validateJWT = async(req = request, res = response, next)=> {
+    const token = req.header('x-token');
+
+    if(!token){
+        return res.status(401).json({
+            msg: 'There is no token in the request',
+        });
+    }
+
+    try{
+        const { uid } = jwt.verify(token, process.env.SECRETORPRIVATEKEY);
+        const admin = await Admin.findById(uid);
+        if(!admin){
+            return res.status(401).json({
+                msg: "Admin does not exist in the database"
+            });
+        }
+
+        if(!admin.state){
+            return res.status(401).json({
+                msg: "Invalid token, admin with false status"
+            });
+        }
+
+        req.admin = admin;
+        next();
+        
+    }catch(e){
+        console.log(e);
+        res.status(401).json({
+            msg: "Invalid token"
+        })
+    }
+} 
