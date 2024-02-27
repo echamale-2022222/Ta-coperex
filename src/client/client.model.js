14bd159a6f542a67d867a6711ce0efee78848341
echamale-2022222 <echamale-2022222@kinal.edu.gt>
2024-02-27 09:55:07 -0600

    Create the partner model with its attributes

---
+++ src/client/client.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const PartnerSchema = mongoose.Schema({
+    name:{
+        type: String,
+        required: [true, "The partner's name is required"]
+    },
+    activitySector:{
+        type: String,
+        required: [true, "Sector activity is mandatory"]
+    },
+    companyImpactLevel:{
+        type: String,
+        required: [true, "The company's impact level is mandatory"]
+    },
+    yearsOfCompanyExperience:{
+        type: Number,
+        required: [true, "The years of experience of the company is mandatory"]
+    },
+    businessCategoryOfTheCompany:{
+        type: String,
+        required: [true, "The business category of the company is mandatory"]
+    },
+    state:{
+        type: Boolean,
+        default: true
+    }  
+});
+
+export default mongoose.model('Partner', PartnerSchema);
